ad6f44961c18e38317ccb19630e3471ab08bb7c7
Amidamaru <thaodt@users.noreply.github.com>
2023-04-05 12:10:51 +0700

    update alacritty conf & keep nvimtree open by default

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -20,7 +20,7 @@ lvim.builtin.terminal.active = false
 lvim.builtin.alpha.active = true
 
 -- close nvimtree when opening a file
-lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
+-- lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
 
 reload "user.plugins"
 reload "user.options"
